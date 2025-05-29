gattaca-com/based-optimism

test: test if supervisor reorgs L2, due to L1 reorg

op-devstack/sysgo/system.go
@@ -92,7 +92,7 @@ func DefaultInteropSystem(dest *DefaultInteropSystemIDs) stack.Option[*Orchestra
 	opt.Add(WithL2CLNode(ids.L2ACL, true, ids.L1CL, ids.L1EL, ids.L2AEL))
 	opt.Add(WithL2CLNode(ids.L2BCL, true, ids.L1CL, ids.L1EL, ids.L2BEL))
 
-	opt.Add(WithSequencer(ids.Sequencer, ids.L2ACL, ids.L1EL, ids.L2AEL))
+	opt.Add(WithSequencer(ids.Sequencer, ids.L1CL, ids.L2ACL, ids.L1EL, ids.L2AEL))
 
 	opt.Add(WithBatcher(ids.L2ABatcher, ids.L1EL, ids.L2ACL, ids.L2AEL))
 	opt.Add(WithBatcher(ids.L2BBatcher, ids.L1EL, ids.L2BCL, ids.L2BEL))

op-test-sequencer/sequencer/backend/work/config/builder.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/backend/work"
+	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/backend/work/builders/l1builder"
 	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/backend/work/builders/noopbuilder"
 	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/backend/work/builders/standardbuilder"
 	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/seqtypes"
@@ -12,6 +13,7 @@ import (
 type BuilderEntry struct {
 	Standard *standardbuilder.Config `yaml:"standard,omitempty"`
 	Noop     *noopbuilder.Config     `yaml:"noop,omitempty"`
+	L1       *l1builder.Config       // L1 is supported only in-process
 }
 
 func (b *BuilderEntry) Start(ctx context.Context, id seqtypes.BuilderID, opts *work.ServiceOpts) (work.Builder, error) {
@@ -20,6 +22,8 @@ func (b *BuilderEntry) Start(ctx context.Context, id seqtypes.BuilderID, opts *w
 		return b.Standard.Start(ctx, id, opts)
 	case b.Noop != nil:
 		return b.Noop.Start(ctx, id, opts)
+	case b.L1 != nil:
+		return l1builder.NewL1Builder(ctx, id, opts, b.L1)
 	default:
 		return nil, seqtypes.ErrUnknownKind
 	}

zheh12/optimism

op-devstack/sysgo/system.go
@@ -66,7 +66,7 @@ func DefaultMinimalSystem(dest *DefaultMinimalSystemIDs) stack.Option[*Orchestra
 
 	opt.Add(WithFaucets([]stack.L1ELNodeID{ids.L1EL}, []stack.L2ELNodeID{ids.L2EL}))
 
-	opt.Add(WithTestSequencer(ids.TestSequencer, ids.L2CL, ids.L1EL, ids.L2EL))
+	opt.Add(WithTestSequencer(ids.TestSequencer, ids.L1CL, ids.L2CL, ids.L1EL, ids.L2EL))
 
 	opt.Add(stack.Finally(func(orch *Orchestrator) {
 		*dest = ids
@@ -163,7 +163,7 @@ func DefaultInteropSystem(dest *DefaultInteropSystemIDs) stack.Option[*Orchestra
 	opt.Add(WithL2CLNode(ids.L2ACL, true, ids.L1CL, ids.L1EL, ids.L2AEL))
 	opt.Add(WithL2CLNode(ids.L2BCL, true, ids.L1CL, ids.L1EL, ids.L2BEL))
 
-	opt.Add(WithTestSequencer(ids.TestSequencer, ids.L2ACL, ids.L1EL, ids.L2AEL))
+	opt.Add(WithTestSequencer(ids.TestSequencer, ids.L1CL, ids.L2ACL, ids.L1EL, ids.L2AEL))
 
 	opt.Add(WithBatcher(ids.L2ABatcher, ids.L1EL, ids.L2ACL, ids.L2AEL))
 	opt.Add(WithBatcher(ids.L2BBatcher, ids.L1EL, ids.L2BCL, ids.L2BEL))

op-test-sequencer/sequencer/backend/work/config/builder.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/backend/work"
+	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/backend/work/builders/fakepos"
 	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/backend/work/builders/noopbuilder"
 	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/backend/work/builders/standardbuilder"
 	"github.com/ethereum-optimism/optimism/op-test-sequencer/sequencer/seqtypes"
@@ -12,6 +13,7 @@ import (
 type BuilderEntry struct {
 	Standard *standardbuilder.Config `yaml:"standard,omitempty"`
 	Noop     *noopbuilder.Config     `yaml:"noop,omitempty"`
+	L1       *fakepos.Config         // L1 is supported only in-process
 }
 
 func (b *BuilderEntry) Start(ctx context.Context, id seqtypes.BuilderID, opts *work.ServiceOpts) (work.Builder, error) {
@@ -20,6 +22,8 @@ func (b *BuilderEntry) Start(ctx context.Context, id seqtypes.BuilderID, opts *w
 		return b.Standard.Start(ctx, id, opts)
 	case b.Noop != nil:
 		return b.Noop.Start(ctx, id, opts)
+	case b.L1 != nil:
+		return fakepos.NewBuilder(ctx, id, opts, b.L1)
 	default:
 		return nil, seqtypes.ErrUnknownKind
 	}
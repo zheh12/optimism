94d997b02f3c5a7ade73addf51af10e58061c3e0
Anton Evangelatov <anton.evangelatov@oplabs.co>
2025-05-14 19:02:07 +0200

    test: test if supervisor reorgs L2, due to L1 reorg

--- op-devstack/dsl/l2_el.go
+++ op-devstack/dsl/l2_el.go
@@ -34,10 +34,10 @@ func (el *L2ELNode) Escape() stack.L2ELNode {
 	return el.inner
 }
 
-func (el *L2ELNode) BlockRefByLabel(label eth.BlockLabel) eth.BlockRef {
+func (el *L2ELNode) BlockRefByLabel(label eth.BlockLabel) eth.L2BlockRef {
 	ctx, cancel := context.WithTimeout(el.ctx, DefaultTimeout)
 	defer cancel()
-	block, err := el.inner.EthClient().BlockRefByLabel(ctx, label)
+	block, err := el.inner.L2EthClient().L2BlockRefByLabel(ctx, label)
 	el.require.NoError(err, "block not found using block label")
 	return block
 }
